1cce45f60875fb98c1b878a5bbfbd56463b98a1f
kevin santana <kevin_santana120@hotmail.com>
2022-10-18 22:11:31 -0500

    i created a new file solicitar fecha de nacimiento as a practice using scanf and print

---
+++ codigos_basicosdec/SolicitarFechaNacimiento.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int diaNacimiento,mesNacimiento,anioNacimiento;
+
+    printf("Bienvenido a nuestro comercio");
+    printf("Nos gustaria mucho conocer Cuando es tu cumpleanos, para poder enviarte un email y felicitarte \n");
+    printf("Por favor, introduce la siguiente informacion :\n");
+    
+    printf("dia de nacimiento: ");
+    scanf("%d",diaNacimiento);
+
+    printf("Mes de nacimiento: ");
+    scanf("%d",mesNacimiento);
+
+    printf("Anio de nacimiento: ");
+    scanf("%d",anioNacimiento);
+
+    return 0;
+}
