fdf274c8c6dbf79475f3886899ac5a35a4ff52ac
kevin santana <kevin_santana120@hotmail.com>
2022-10-24 23:23:47 -0500

    creamos un script practicand arrays y punteros

---
+++ c_avanzado/Dimensiones_punteros_y_Arrays.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int identificadoresEmpleados[10];
+	&identificadoresEmpleados[0];//direccion del primer elemento en la primera posicion
+	identificadoresEmpleados;//obtenemos la direccion de memoria del elemento, el primer elemento del array
+	&identificadoresEmpleados[8];
+	identificadoresEmpleados+8;//obtenemos la direccion de memoria del noveno elemento
+	
+	&identificadoresEmpleados[8];
+	identificadoresEmpleados[8];//obtenemos el noveno elemento
+	*(identificadoresEmpleados+8);//obtenemos el noveno elemento
+	//esta ultimo enunciado es el mas eficiente en compilacion
+}
