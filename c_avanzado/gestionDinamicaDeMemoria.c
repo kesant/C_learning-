50e18cce1dee855107289af11906cb46e5fec9a9
kevin santana <kevin_santana120@hotmail.com>
2022-10-29 00:48:17 -0500

    practicas de gestion dinamica de memoria

--- c_avanzado/gestionDinamicaDeMemoria.c
+++ c_avanzado/gestionDinamicaDeMemoria.c
@@ -10,6 +10,16 @@ int main(){
 //malloc nos devolvera un puntero avoid que necesitaremos convertir de un puntero a entero para poder trabajar   
 //con (int*)	
 	int *empleados=(int*) malloc(500*sizeof(int));
+//Arrays dinamicos en C
+	int numProductos;
+	printf("Cuantos productos vendiste ayer ?:");
+	scanf("%d",&numProductos);
+	float *numProductosVendidos=(float*)malloc(numProductos*sizeof(float));
+	for (int i =0;i<numProductos;i++){
+		printf("Introduce el precio del producto %d :",i+);
+		scanf("%f",&numProductosVendidos[i]); 
+    }
+//como liberar memoria asignada en C 
 
 	return 0;
 }
