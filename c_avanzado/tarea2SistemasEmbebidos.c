c80244428fbf0b3edcc46b797ebd98f6cf708489
kevin santana <kevin_santana120@hotmail.com>
2022-10-30 00:01:21 -0500

    termine la tarea de punteros , arraysestaticos y dinamicos de la tarea 2

--- c_avanzado/tarea2SistemasEmbebidos.c
+++ c_avanzado/tarea2SistemasEmbebidos.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-float calculoDescuento(char inicialProducto[],float costo[],int cantidadProductos);
+float calculoDescuento(char inicialProducto[],float costo[],int cantidadProductos,float *costoTotal1,float *costoDescuento,float *descuento);
+void informe(char inicialProducto[],int cantidadProductos,float costoValor1,float costoDescuento,float descuento);
 int main (){
     puts("*************************BIENVENIDO AL DISPENSADOR DE SODAS****************************");
     puts("                             MENU                                        ");
@@ -9,17 +10,30 @@ int main (){
     puts("B.-Coca cola\t0.35$");
     puts("C.-Inca cola\t0.20$");
     puts("D.-Fanta\t0.25$");
-    puts("E.-Sprite\t220.30$\n");
-
+    puts("E.-Sprite\t0.30$\n");
+    puts("AL COMPRAR 5 PRODUCTOS IGUALES OBTIENE UN DESCUENTO DEL 15 PORCIENTO");
     int cantidadProductos,terminador=1;
     char inicialProducto[]={'A','B','C','D','E'};
     float costo[]={0.25,0.35,0.20,0.25,0.30};
     char producto;
-
+    float costoTotal1,costoDescuento,descuento;
+    char confirmacion;
     puts("Ingresa la cantidad de productos que quiere comprar: ");
     scanf("%d",&cantidadProductos);//ingreso de datos por el usuario
-    float *productos=(float*)malloc(cantidadProductos*sizeof(float));
-    puts("Ingrese la opcion del producto que desee comprar: ");
+    //ESTAMOS UTILIZANDO MANEJO DINAMICO DE ARRAYS EN MEMORIA PARA REESTRABLECER EL TAMANIO DEL ARRAY CON LAS FUNCIONES MALLOC Y REALLOC
+    char *productos=(char*)malloc(cantidadProductos*sizeof(char));
+    while(terminador){
+        printf("Usted quiere comprar %d bebidas, desea continuar ?Y/N (Mayusculas only): \n",cantidadProductos);
+        scanf(" %c",&confirmacion);
+        if (confirmacion=='Y'){
+            terminador=0;
+        }else{
+            puts("Ingresa la cantidad de productos que quiere comprar: ");
+            scanf("%d",&cantidadProductos);//ingreso de datos por el usuario
+            productos= realloc(productos,cantidadProductos*sizeof(char));    
+        }
+    }
+    puts("Ingrese la opcion del producto que desee comprar(Mayusculas only): ");
     for(int i=0;i<cantidadProductos;i++){
         printf("Producto # %d\n",i+1);
         do{
@@ -30,12 +44,16 @@ int main (){
         }while(strchr(inicialProducto,producto)==NULL);
         productos[i]=producto;
     }
-    float costoValor=calculoDescuento(inicialProducto, costo,cantidadProductos);
-    printf("%.2f",costoValor);
+
+
+
+
+    calculoDescuento(productos,costo,cantidadProductos,&costoTotal1,&costoDescuento,&descuento);
+    informe(productos,cantidadProductos,costoTotal1,costoDescuento,descuento);
 }
 
 
-float calculoDescuento(char inicialProducto[],float costo[],int cantidadProductos){
+float calculoDescuento(char inicialProducto[],float costo[],int cantidadProductos,float *costoTotal1,float *costoDescuento,float *descuento){
     int A=0,B=0,C=0,D=0,E=0,contadorDescuento=0;
     float costoTotal=0;
     for (int i=0;i<cantidadProductos;i++){
@@ -79,8 +97,47 @@ float calculoDescuento(char inicialProducto[],float costo[],int cantidadProducto
         }
     }
     if (contadorDescuento>0){
-        costoTotal=costoTotal*1.15;
+        *costoDescuento=costoTotal*0.85;
+        *descuento=costoTotal*0.15;
+        *costoTotal1=costoTotal;
+    }else{
+        *costoDescuento=0.0;
+        *descuento=0.0;
+        *costoTotal1=costoTotal;
     }
-    return costoTotal;
+    return 0;
+
+}
+void informe(char inicialProducto[],int cantidadProductos,float costoValor1,float costoDescuento,float descuento){
+    puts("#############################################################");
+    puts("\t\t***********************Factura***********************\t\t");
+    printf("Tus productos son : \n");
+    puts("Producto\tPrecio");
+        for (int i=0;i<cantidadProductos;i++){
+        switch(inicialProducto[i]){
+            case 'A': 
+            puts("Pepsi\t\t0.25$");
+            break;
+            case 'B':
+            puts("Coca cola\t0.35$");       
+            break;
+            case 'C':
+            puts("Inca cola\t0.20$");
+            break;
+            case 'D':
+            puts("Fanta\t\t0.25$");
+            break;
+            case 'E':
+            puts("Sprite\t\t0.30$");
+            break;
+            default: /*accion por defecto */ break;
+        }
+    }
+    puts("#############################################################");
+    printf("total\t\t%.2f\n$",costoValor1);
+    printf("descuento\t%.2f\n$",descuento);
+    printf("total a pagar\t%.2f\n$",costoDescuento);
+    puts("#############################################################");
+    printf("\t\tGRACIAS POR SU COMPRA");
 
 }
